d6aa8f2d24be353ef1e50b93273a2a427fefbe01
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-03-05 12:16:22 -0800

    Define Feed#search and helper methods

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,19 +2,45 @@ class Feed < ApplicationRecord
   has_many :feed_categories
   has_many :categories, through: :feed_categories
   has_many :entries
-  
+
   def self.search(search_term)
-    if Feed.find_by(search_term) == search_term
-      @feed = Feed.find_by(search_term)
+
+    @feed = add_scheme_to_search_url(search_term)
+
+    if @feed.nil?
+      feed = find_feed_from_url(search_term)
+
+      if !feed.nil?
+        feed = fetch_and_parse_feed(feed)
+
+        @feed = Feed.new(
+          url: feed.url,
+          description: feed.description,
+          title: feed.title,
+          categories: Category.all.sample(1)
+          )
+        end
     else
-      feed = Feedbag.find(search_term).first
-      feed = Feedjira::Feed.fetch_and_parse(feed) if !feed.empty?
-      @feed = Feed.new(
-        url: feed.url,
-        description: feed.description,
-        title: feed.title,
-        categories: Category.all.sample(1)
-      )
+      @feed
     end
+  end
+
+  def self.add_scheme_to_search_url(search_term)
+    http = Feed.find_by(url: "http://#{search_term}")
+    https = Feed.find_by(url: "https://#{search_term}")
+
+    if http
+      @feed = http
+    elsif https
+      @feed = https
+    end
+  end
+
+  def self.find_feed_from_url(url)
+    Feedbag.find(url).first
+  end
+
+  def self.fetch_and_parse_feed(feed)
+    Feedjira::Feed.fetch_and_parse(feed)
   end
 end
