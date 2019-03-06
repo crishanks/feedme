91779045dce38d1d2dd2c8eef58bf7b4bf26afb5
github username <crishanks12@gmail.com>
2019-03-06 09:37:21 -0800

    sessions and authentication updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,23 @@
 class SessionsController < ApplicationController
 
-    def new
-    end
+  def new
+  end
 
-    def create
-        user = User.find_by(name: params[:name])
-        if user
-            session[:name] = params[:name]
-            redirect_to feeds_path
-        else
-            flash[:notice] = "Invalid Login"
-            redirect_to login_path
-        end
-      
-    end
-
-    def destroy
-      session.clear
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = params[:user_id]
+      redirect_to feeds_path
+    else
+      flash[:notice] = "Invalid Login"
       redirect_to login_path
     end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+
+  #relationships
   has_many :feed_subscriptions
   has_many :feeds, through: :feed_subscriptions
   has_many :feed_categories
   has_many :tags
   has_many :entries, through: :feeds
+
+  #authentication
+  has_secure_password
+
+  #validations
+  
 end
