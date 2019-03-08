e2c620d91ab3124f7ab2c4211ec6d757455efc31
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-03-07 18:58:46 -0800

    Add tagging entries feature (routes, methods, views)

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,18 @@ class User < ApplicationRecord
     end
   end
 
+  def update_or_create_tags(params_hash)
+    entry = Entry.find(params_hash[:id])
+
+    if params_hash[:tag_ids].size > 1
+      params_hash[:tag_ids].each do |tag_id|
+        EntryTag.create(entry: entry, tag_id: tag_id)
+      end
+    end
+
+    if !params_hash[:tags][:name].empty?
+      tag = Tag.find_or_create_by(user: self, name: params_hash[:tags][:name])
+      EntryTag.create(tag: tag, entry: entry)
+    end
+  end
 end
