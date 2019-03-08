d5dd3d324bb36d421f0c68b970f170e0ca3b9f6f
github username <crishanks12@gmail.com>
2019-03-07 19:32:38 -0800

    fixed validations for tags and feed categories, reformatted tags and categories display

--- app/models/entry_tag.rb
+++ app/models/entry_tag.rb
@@ -1,4 +1,6 @@
 class EntryTag < ApplicationRecord
     belongs_to :entry
     belongs_to :tag
+
+    validates :tag_id, uniqueness: { scope: :entry_id }, presence: true
 end
--- app/models/feed_to_category_helper.rb
+++ app/models/feed_to_category_helper.rb
@@ -1,4 +1,6 @@
 class FeedToCategoryHelper < ApplicationRecord
     belongs_to :feed
     belongs_to :feed_category
+
+    validates :feed_category_id, uniqueness: { scope: :feed_id }, presence: true
 end
